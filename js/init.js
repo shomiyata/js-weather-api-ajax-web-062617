fa484048b49ca7de71a1a99ff94fa5b93863ba2e
github username <github email address>
2017-08-16 14:20:27 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "97605a88d2883381";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
+  	console.log(json)
     var data = generateDataSet(getHours(json), getFahrenheits(json));
     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
   });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,41 @@
 function getFahrenheits(result){
-  // Your code goes here
+	let degrees = result.hourly_forecast.map(hour => hour.temp.english)
+	return degrees
 }
 
 function getHours(result){
-  // Your code goes here
+	let hours = result.hourly_forecast.map(hour => hour.FCTTIME.hour)
+	return hours
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+	let allData = 
+	{
+    labels: labels,
+    datasets: [{
+    	label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+	    data: data
+    }]
+  }
+  return allData
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+	$.ajax({
+		url: endpoint,
+		dataType: 'jsonp',
+		success: success
+	})
+
+	// let request = fetch(endpoint)
+	// .then(function(data){
+ //     return data.json()})
+	// .then(function(data){
+	//  success(data)})
 }
